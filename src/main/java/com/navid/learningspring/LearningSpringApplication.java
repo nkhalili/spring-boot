9f43edb5986f97677af17539b4a9b5a039d8e0dc
Navid Khalili <nkhalili@live.com>
2020-07-27 22:13:42 -0400

    spring data sample

--- src/main/java/com/navid/learningspring/LearningSpringApplication.java
+++ src/main/java/com/navid/learningspring/LearningSpringApplication.java
@@ -1,7 +1,13 @@
 package com.navid.learningspring;
 
+import com.navid.learningspring.data.entity.Room;
+import com.navid.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringApplication {
@@ -10,4 +16,17 @@ public class LearningSpringApplication {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
 
+
+	// this is a sample code to make sure code works
+	@RestController
+	@RequestMapping("/rooms")
+	public class RoomController{
+		@Autowired
+		private RoomRepository roomRepository;
+
+		@GetMapping
+		public Iterable<Room> getRooms(){
+			return this.roomRepository.findAll();
+		}
+	}
 }
