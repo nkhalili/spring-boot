33518fefc56ac775e0c8e8b7114d34621b3f9f2d
Navid Khalili <nkhalili@live.com>
2020-07-28 16:02:32 -0400

    added guest, reservation

--- src/main/java/com/navid/learningspring/LearningSpringApplication.java
+++ src/main/java/com/navid/learningspring/LearningSpringApplication.java
@@ -1,8 +1,13 @@
 package com.navid.learningspring;
 
+import com.navid.learningspring.data.entity.Guest;
+import com.navid.learningspring.data.entity.Reservation;
 import com.navid.learningspring.data.entity.Room;
+import com.navid.learningspring.data.repository.GuestRepository;
+import com.navid.learningspring.data.repository.ReservationRepository;
 import com.navid.learningspring.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +32,30 @@ public class LearningSpringApplication {
 		@GetMapping
 		public Iterable<Room> getRooms(){
 			return this.roomRepository.findAll();
+		}
+	}
+
+	@RestController
+	@RequestMapping("/guests")
+	public class GuestController{
+		@Autowired
+		private GuestRepository guestRepository;
+
+		@GetMapping
+		public Iterable<Guest> getGuests(){
+			return this.guestRepository.findAll();
+		}
+	}
+
+	@RestController
+	@RequestMapping("/reservations")
+	public class ReservationController{
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@GetMapping
+		public Iterable<Reservation> getReservations(){
+			return this.reservationRepository.findAll();
 		}
 	}
 }
---
+++ src/main/java/com/navid/learningspring/data/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.navid.learningspring.data.repository;
+
+import com.navid.learningspring.data.entity.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
